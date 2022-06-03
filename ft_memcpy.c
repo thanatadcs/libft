a67b5b358993dfe02084ce16a6586798b3996252
thanatadcs <thanatad.cs@gmail.com>
2022-06-03 23:58:43 +0700

    Prevent deref null ptr in ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tanukool <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 16:09:32 by tanukool          #+#    #+#             */
-/*   Updated: 2022/06/01 16:30:19 by tanukool         ###   ########.fr       */
+/*   Updated: 2022/06/03 23:56:57 by tanukool         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (dst == 0 || src == 0)
+		return (dst);
 	while (i < n)
 	{
 		((char *) dst)[i] = ((char *) src)[i];

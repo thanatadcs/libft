e6544c76cdbf8c68d93c560bdae983229c4e0a79
thanatadcs <thanatad.cs@gmail.com>
2022-06-08 11:13:24 +0700

    Added handle case for null input in ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tanukool <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 16:09:32 by tanukool          #+#    #+#             */
-/*   Updated: 2022/06/08 10:58:55 by tanukool         ###   ########.fr       */
+/*   Updated: 2022/06/08 11:11:16 by tanukool         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (n == 0 || dst == src)
+		return (dst);
 	while (i < n)
 	{
 		((char *) dst)[i] = ((char *) src)[i];

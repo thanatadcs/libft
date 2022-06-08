41d37b1c28e0072541516030146e24c44adc85f9
thanatadcs <thanatad.cs@gmail.com>
2022-06-08 11:33:17 +0700

    Added null protection to ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tanukool <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 10:50:23 by tanukool          #+#    #+#             */
-/*   Updated: 2022/06/04 00:21:51 by tanukool         ###   ########.fr       */
+/*   Updated: 2022/06/08 11:32:32 by tanukool         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	return_len;
 	char	*to_return;
 
+	if (s == 0)
+		return (0);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
 		return (ft_calloc(1, 1));

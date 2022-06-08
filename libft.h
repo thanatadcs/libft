7141b1caba31c1c4226bbe4aabfbb126d8efc1e7
thanatadcs <thanatad.cs@gmail.com>
2022-06-08 16:31:18 +0700

    Added ft_lstiter.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tanukool <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 15:38:34 by tanukool          #+#    #+#             */
-/*   Updated: 2022/06/08 15:14:50 by tanukool         ###   ########.fr       */
+/*   Updated: 2022/06/08 16:01:21 by tanukool         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif

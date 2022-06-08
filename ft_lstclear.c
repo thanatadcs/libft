3ed87ff307a38eb641c3fb54fce7e0a146823778
thanatadcs <thanatad.cs@gmail.com>
2022-06-08 15:32:16 +0700

    Added ft_lstclear.c

---
+++ ft_lstclear.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanukool <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 15:15:00 by tanukool          #+#    #+#             */
+/*   Updated: 2022/06/08 15:31:09 by tanukool         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*cur_node;
+	t_list	*prev_node;
+
+	if (lst == 0)
+		return ;
+	else if (*lst == 0)
+		free(lst);
+	cur_node = *lst;
+	while (cur_node)
+	{
+		prev_node = cur_node;
+		cur_node = cur_node->next;
+		ft_lstdelone(prev_node, del);
+	}
+	*lst = 0;
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tanukool <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 15:38:34 by tanukool          #+#    #+#             */
-/*   Updated: 2022/06/08 15:06:01 by tanukool         ###   ########.fr       */
+/*   Updated: 2022/06/08 15:14:50 by tanukool         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,6 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
 
 #endif
